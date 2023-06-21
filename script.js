6bca356657bc136f4ce991a9d75c0a23f71f6f46
Tyler D <130164977+tdaniel12299@users.noreply.github.com>
2023-06-21 07:01:27 -0400

    added javascript, evaluation changed to eq

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+let result = document.getElementById('result');
+
+
+const appendValue = (value) => {
+    result.value += value;
+};
+
+const clearResult = () => {
+    result.value = '';
+};
+const backspace = () => {
+    result.value = result.value.slice(0, -1);
+};
+
+const eq = () => {
+try {
+    result.value = eval(result.value);
+} catch (error) {
+    result.value = 'Error';
+}
+}
+
 
